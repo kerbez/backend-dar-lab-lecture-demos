85603eafff5a026bb28ca461967990ef63f785fe
KerbezOrazgaliyeva <160107140@stu.sdu.edu.kz>
2019-04-24 02:36:20 +0600

    lab3 task2

--- src/main/scala/lab3/Boot.scala
+++ src/main/scala/lab3/Boot.scala
@@ -17,11 +17,50 @@ object Boot extends App{
   println()
   println("--------task2--------")
 
+  def isInt(op1: String, op: String, op2: String): Option[Int] =
+    if((op1 matches "\\d+") && (op1 matches "\\d+")) Some(op1.toInt) else None
+
+
+  def readStr(str: String): Option[String] ={
+    str match{
+      case "/" => Some("/")
+      case "+" => Some("+")
+      case "-" => Some("-")
+      case "*" => Some("*")
+      case _ => None
+    }
+  }
+
   def readInt(str: String): Option[Int] =
     if(str matches "\\d+") Some(str.toInt) else None
 
-//  def calculator(op1: String, op: String, op2: String){
-//    readInt(op1).flatMap(readInt(op2))
-//  }
+  def res(op1: Int, op: String, op2: Int): Option[Int] ={
+    if(op == "/" && op2 == 0) {
+      println("here")
+      None
+    }
+    else
+    op match {
+      case "/" => Some(op1/op2)
+      case "+" => Some(op1+op2)
+      case "*" => Some(op1*op2)
+      case "-" => Some(op1-op2)
+    }
+  }
+
+  def calculator(op1: String, op: String, op2: String): Unit ={
+    if((readInt(op1) != None) && (readInt(op2) != None)){
+      if(readStr(op) != None){
+        println(res(readInt(op1).get, readStr(op).get, readInt(op2).get))
+      }
+    }
+  }
+
+  calculator("4", "+", "2")
+  calculator("4", "/", "0")
+  calculator("4", "*", "2")
+  calculator("4", "-", "2")
+  calculator("4", "/", "2")
+
 
 }
